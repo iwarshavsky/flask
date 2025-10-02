0b502f49609b8e1355c433dc37ddfe7fc88032b6
iwarshavsky <52420845+iwarshavsky@users.noreply.github.com>
2025-10-02 03:21:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def redirect_all(path):
-    return redirect("https://example.com", code=302)  # Change to your desired URL
+    return redirect("https://jobs.eu.lever.co/mobileye/d1580dbc-9de7-4ade-90e1-6ba1817806dc", code=302)  # Change to your desired URL
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
