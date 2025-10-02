fca888ab3fb1a4bbc1633b1110e94dccbd69ef36
iwarshavsky <52420845+iwarshavsky@users.noreply.github.com>
2025-10-02 19:42:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+import logging
 from flask import Flask, redirect, request
 import os
 
+
 app = Flask(__name__)
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+@app.before_request
+def log_request():
+    app.logger.info(f"Request: {request.method} {request.url}")
 
 @app.route('/<path:url_path>')
 def smart_redirect(url_path):
+    
     # Reconstruct full destination URL, default to https
     destination = f"https://{url_path}"
     if request.query_string:
