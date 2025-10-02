1dd6ae1a25093d3e60c1b071e9aef884ffc8bf1f
iwarshavsky <52420845+iwarshavsky@users.noreply.github.com>
2025-10-02 03:25:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, request, abort
 import os
+import urllib.parse
 
 app = Flask(__name__)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def redirect_all(path):
-    return redirect("https://jobs.eu.lever.co/mobileye/d1580dbc-9de7-4ade-90e1-6ba1817806dc", code=302)  # Change to your desired URL
+@app.route('/', methods=['GET'])
+def forward_to_next():
+    next_url = request.args.get('next')
+    # Optional: Validate the URL (basic check)
+    if not next_url or not urllib.parse.urlparse(next_url).scheme:
+        abort(400, description="Missing or invalid 'next' parameter")
+    return redirect(next_url)
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
